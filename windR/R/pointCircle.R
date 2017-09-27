f344baef6ca1a2c77a8860b5d7499f913c5d13b0
krietsch <jkrietsch@orn.mpg.de>
2017-09-27 14:54:05 +0200

    update

--- windR/R/pointCircle.R
+++ windR/R/pointCircle.R
@@ -3,93 +3,76 @@
 #'
 #' This function caclulates the position of points in a circle around a start point. The circle is based on the distance of the second point that is provided. This allows for example to compare the the wind conditions in all directions.
 #'
-#' @param x
-#' @param y
-#' @param x2
-#' @param y2
-#' @param wind
+#' @param x Longitude of point 1
+#' @param y Latitude of point 1
+#' @param x2 Longitude of point 2
+#' @param y2 Latitude of point 2
+#' @param id Bird ID
+#' @param point_id Point ID
+#' @param pointN The number of points that should be created on the circle
+#' @param PROJ The projection of the points (should be equal area)
 #'
 #' @return
 #' @export
 #'
 #' @examples
 #'
+#' id       = 'bird1'
+#' point_id = 1
+#' x        = 10
+#' y        = 10
+#' x2       = 100
+#' y2       = 600
+#' pointN   = 36
+#' PROJ     = '+proj=laea +lat_0=90 +lon_0=-156.653428 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0 '
+#'
+#' dp = pointCircle(x, y, x2, y2, id, point_id, pointN = 36, PROJ)
+#'
+#' # visualization of the example
+#' PS  = SpatialPointsDataFrame(dp[1, .(x,y)], dp[1, .(id, pointType)], proj4string = CRS(PROJ), match.ID = TRUE)
+#' PS2 = SpatialPointsDataFrame(dp[, .(x2,y2)], dp[, .(id, pointType)], proj4string = CRS(PROJ), match.ID = TRUE)
+#'
+#' plot(PS2[PS2@data$pointType == 'estimated', ], col = 'red')         # estimated points
+#' plot(PS2[PS2@data$pointType == 'real', ], col = 'blue', add = TRUE) # second point
+#' plot(PS, add = TRUE)                                                # first point
 
+pointCircle = function(x, y, x2, y2, id, point_id, pointN = 36, PROJ){
 
-# Packages, settings
-sapply(c('rgdal', 'RSQLite', 'magrittr', 'graticule', 'rgeos', 'ggplot2', 'raster', 'data.table', 'zoo', 'foreach'),
-       function(x) suppressPackageStartupMessages(require(x , character.only = TRUE, quietly = TRUE) ) )
+  dt = data.table(id = id,
+                  point_id = point_id,
+                  x = x,
+                  y = y,
+                 x2 = x2,
+                 y2 = y2,
+                 pointType = 'real' )
 
-PROJ   = '+proj=laea +lat_0=90 +lon_0=-156.653428 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0 '
+  # distance between the two points
+  P_dist = sqrt(sum((c(x, y) - c(x2, y2))^2))
 
+  # create spatial points
+  PS = SpatialPointsDataFrame(dt[, .(x,y)], dt[, .(id)], proj4string = CRS(PROJ), match.ID = TRUE)
+  PS2 = SpatialPointsDataFrame(dt[, .(x2,y2)], dt[, .(id)], proj4string = CRS(PROJ), match.ID = TRUE )
 
-
-# load tracking data
-load('//ds/grpkempenaers/Hannes/sea_ice_flights/DATA/P_Speed.RData')
-
-d = d[point_id == 1]
-d = d[bird_patch_id == '42700_1']
-
-
-PS = SpatialPointsDataFrame(d[, .(x,y)], d[, .(bird_patch_id, bird_id, patchid, datetime_)], proj4string = CRS(PROJ), match.ID = TRUE )
-
-
-plot(PS)
-
-
-PS[, dist       := sqrt(sum((c(x, y) - c(x2, y2))^2)) , by = 1:nrow(PS)]
-
-Barrow250 = gBuffer(PS, width = 250000, quadsegs = 100)
-
-
-
-
-windCircle = function(x, y, x2, y2, datetime_, TbtwPoints){
-
-d = data.table( x = d$x,
-                y = d$y,
-               x2 = d$x2,
-               y2 = d$y2,
-               type = 'real',
-               datetime_ = d$datetime_)
-
-
-
-  # distance between points
-  d[, dist       := sqrt(sum((c(x, y) - c(x2, y2))^2)) , by = 1:nrow(d)]
-
-  PS = SpatialPointsDataFrame(d[, .(x,y)], d[, .(datetime_)], proj4string = CRS(PROJ), match.ID = TRUE )
-  PS2 = SpatialPointsDataFrame(d[, .(x2,y2)], d[, .(datetime_)], proj4string = CRS(PROJ), match.ID = TRUE )
-
-  PS_buffer = gBuffer(PS, width = d$dist, quadsegs = 10)
-
+  # create a circle around the point with the distance of the second point
+  PS_buffer = gBuffer(PS, width = P_dist, quadsegs = 10)
   PS_line = as(PS_buffer, "SpatialLines")
 
-  PS_points = spsample(PS_line, n = 36, type = 'regular')
+  # sample points on the circle
+  PS_points = spsample(PS_line, n = pointN, type = 'regular')
 
   dE = as.data.table(PS_points)
 
-
-
-  d2 = data.table(x  = rep(d$x, nrow(dE)),
-                  y  = rep(d$y, nrow(dE)),
+  d2 = data.table(id = dt$id,
+                  point_id = dt$point_id,
+                  x  = rep(dt$x, nrow(dE)),
+                  y  = rep(dt$y, nrow(dE)),
                   x2 = dE$x,
                   y2 = dE$y,
-                  type = rep('estimated', nrow(dE)),
-                  datetime_ = rep(d$datetime_, nrow(dE)))
+                  pointType = rep('estimated', nrow(dE)) )
 
-  d[, dist := NULL]
+  d = rbind(dt, d2)
 
-  d = rbind(d, d2)
-
-
-  plot(PS_buffer)
-  plot(PS, add = TRUE)
-
-  plot(PS_points, add = TRUE, col = 'red')
-  plot(PS2, add = TRUE, col = 'blue')
-
-
+  return(d)
 
 }
 
