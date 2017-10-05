864b866f76b5f36063a524c0d7252069ce8a7360
krietsch <jkrietsch@orn.mpg.de>
2017-10-05 09:49:38 +0200

    .

--- R/pointCircle.R
+++ R/pointCircle.R
@@ -40,32 +40,32 @@ pointCircle = function(lon, lat, lon2, lat2, pointN = 36, PROJ){
 									lon2 = lon2,
 									lat2 = lat2,
 									pointType = 'real' )
-	
+
 	# distance between the two points
 	P_dist = sqrt(sum((c(lon, lat) - c(lon2, lat2))^2))
-	
+
 	# create spatial points
-	
+
 	PS = SpatialPointsDataFrame(dt[, .(lon,lat)], dt[, .(pointType)], proj4string = CRS(PROJ), match.ID = TRUE)
 	PS2 = SpatialPointsDataFrame(dt[, .(lon2,lat2)], dt[, .(pointType)], proj4string = CRS(PROJ), match.ID = TRUE )
-	
+
 	# create a circle around the point with the distance of the second point
 	PS_buffer = gBuffer(PS, width = P_dist, quadsegs = 10)
 	PS_line = as(PS_buffer, "SpatialLines")
-	
+
 	# sample points on the circle
 	PS_points = spsample(PS_line, n = pointN, type = 'regular')
-	
+
 	dE = as.data.table(PS_points)
-	
+
 	d2 = data.table(lon  = rep(dt$lon, nrow(dE)),
 									lat  = rep(dt$lat, nrow(dE)),
 									lon2 = dE$x,
 									lat2 = dE$y,
 									pointType = rep('estimated', nrow(dE)) )
-	
-	d = rbind(dt, d2)
-	
+
+	d = rbindlist(list(dt, d2), use.names =  TRUE)
+
 	d
 
 }
