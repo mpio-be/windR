5438ed9cc18de66f8f32fd38f395e9270d419c0b
krietsch <jkrietsch@orn.mpg.de>
2018-03-23 14:01:44 +0100

    added example

--- R/getWind.R
+++ R/getWind.R
@@ -1,6 +1,6 @@
-#' get Wind (data)
+#' Connect points to closest wind data in space and time
 #'
-#' This function connects points with the closest wind data in space.
+#' This function takes given points (and their datetime) and connects them with the closest wind data in space.
 #'
 #' @param x Longitude of the point
 #' @param y Latitude of the point
@@ -11,6 +11,36 @@
 #' @export
 #'
 #' @examples
+#' # Load example wind data
+#' PROJ = '+proj=laea +lat_0=90 +lon_0=-156.653428 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0 '
+#' w = readRDS('./data/ERA_Interrim_850mb_4_7_June_2014.RDS')
+#' setkey(w, datetime_)
+#'
+#' # Create example points within the boarders of the wind data (space and time)
+#' library(raster)
+#' library(rgeos)
+#' # Get extent of the wind data, defining the extent of the map
+#' w1 = w[datetime_ == unique(w$datetime_[1])]
+#' wp = SpatialPoints(cbind(w1$x, w1$y), proj4string = CRS(PROJ))
+#' datBdry = gEnvelope(wp)
+#'
+#' ext   = extent(datBdry)
+#' x_ext = seq(ext[1], ext[2])
+#' y_ext = seq(ext[3], ext[4])
+#'
+#' n_points = 100
+#'
+#' d = data.table(point_id  = 1:n_points,
+#'                x         = sample(x_ext, n_points, replace=TRUE),
+#'                y         = sample(y_ext, n_points, replace=TRUE),
+#'                datetime_ = c(sample(w$datetime_, n_points)) + 60*60) # existing wind data + 1 hour
+#'
+#' # Assign closest datetime of wind data
+#' setkey(d, point_id)
+#' d[, w_date := closestDatetime(datetime_, unique(w$datetime_)), by = point_id]
+#'
+#' # Assign closest wind data
+#' d[, c('u', 'v') := getWind(x = x, y = y, w = w[J(w_date), nomatch=0L], PROJ), by = point_id]
 
 getWind = function(x, y , w, PROJ) {
 
@@ -20,3 +50,4 @@ getWind = function(x, y , w, PROJ) {
   return(list(o$u[1], o$v[1]))
 
 }
+
