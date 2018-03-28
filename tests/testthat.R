2ece80989b6453155dab2f399eff8df81466e871
krietsch <jkrietsch@orn.mpg.de>
2018-03-28 16:46:21 +0200

    -

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(windR)
+
+test_check("windR")
---
+++ tests/testthat/test_bearing.R
@@ -0,0 +1,26 @@
+context('bearing')
+
+
+
+test_that('bearing is double', {
+
+  expect_type( bearing(x = 0, y = 0, x2 = 2, y2 = 2),  'double' )
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
