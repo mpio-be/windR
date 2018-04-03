56bf94e18f476945dcea6b3ef8500c2b1bc09f7d
krietsch <jkrietsch@orn.mpg.de>
2018-04-03 17:12:49 +0200

    added more tests

--- tests/testthat/test-windSupport.R
+++ tests/testthat/test-windSupport.R
@@ -13,6 +13,8 @@ test_that('windSupport gives correct wind support and crosswind', {
 
   expect_equal( windSupport(g_direction = 0, g_speed = 15, w_direction = 1, w_speed = 5),  2.701512, tolerance = .000001 )
   expect_equal( windSupport(g_direction = 0, g_speed = 15, w_direction = 3, w_speed = 10),  -9.899925, tolerance = .000001 )
+  expect_equal( windSupport(g_direction = 0, g_speed = 15, w_direction = -3, w_speed = 10),  -9.899925, tolerance = .000001 )
+  expect_equal( windSupport(g_direction = 0, g_speed = 15, w_direction = 6, w_speed = 10),  9.601703, tolerance = .000001 )
   expect_equal( windSupport(g_direction = 0, g_speed = 15, w_direction = 1, w_speed = 5, crosswind = TRUE),  4.207355, tolerance = .000001 )
   expect_output( windSupport(g_direction = NA, g_speed = 15, w_direction = 1, w_speed = 5), NA)
   expect_output( windSupport(g_direction = NA, g_speed = 15, w_direction = 1, w_speed = 5, crosswind = TRUE), NA)
